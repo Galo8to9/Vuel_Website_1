5b2433800c9e2694e0b9edf9f93831931cd68f06
Galo8to9 <rafael.filipe.galo@gmail.com>
2024-05-26 16:04:55 +0100

    New Update

--- components/cards/VuelPostCard.tsx
+++ components/cards/VuelPostCard.tsx
@@ -97,20 +97,31 @@ const VuelPostCard = ({
                         className='cursor-pointer object-contain'
                         />
                     </div>
-
-                    {isComment && comments.length > 0 && (
-                        <Link href={`/vuelpost/${id}`}>
-                            <p className='mt-1 text-subtle-medium text-gray-1'>
-                                {comments.length} repl{comments.length > 1 ? "ies" : "y"}
-                            </p>
-                        </Link>
-                    )}
                     </div>
                 </div>
+                
             </div>
-
-       
         </div>
+        {comments.length > 0 && (
+                       <div className='ml-1 mt-3 flex items-center gap-2'>
+                       {comments.slice(0, 2).map((comment, index) => (
+                         <Image
+                           key={index}
+                           src={comment.author.image}
+                           alt={`user_${index}`}
+                           width={24}
+                           height={24}
+                           className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
+                         />
+                       ))}
+             
+                       <Link href={`/vuelpost/${id}`}>
+                         <p className='mt-1 text-subtle-medium text-gray-1'>
+                           {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                         </p>
+                       </Link>
+                     </div>
+                    )}
         {!isComment && community && (
           <Link href={`/communities/${community.id}`} className='mt-5 flex item-center'>
             <p className='text-subtle-medium text-gray-1'>
